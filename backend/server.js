ff2db815115f5e9c02f7e88de1491e8d99888a6b
Nkechi-Christabel <udenkwornkechi@gmail.com>
2024-06-27 17:09:39 +0100

    Updated one of the expected attributes for user registration, cors port, and journals schema.

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,23 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import router from './routes/index';
+import router from './routes/index.js';
 import { configDotenv } from 'dotenv';
 import cors from 'cors';
 
 configDotenv();
 
-
 const app = express();
 const port = process.env.PORT;
 
 const corsOptions = {
-    origin: 'http://localhost:3000',
-    optionsSuccessStatus: 200
+  origin: 'http://localhost:5173',
+  optionsSuccessStatus: 200,
 };
-  
+
 app.use(cors(corsOptions));
 
 app.use(bodyParser.json());
 app.use('/', router);
-
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
