a7dec4a7ebb669463f9dde43c1ae73d43dd9bc76
Bamiwo Adebayo <bammietop@gmail.com>
2024-06-29 14:07:13 +0100

    Added Documentation to the Backend

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ import router from './routes/index.js';
 import { configDotenv } from 'dotenv';
 import cors from 'cors';
 import morgan from 'morgan';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
 
 configDotenv();
 
@@ -14,6 +16,41 @@ const corsOptions = {
   origin: 'http://localhost:5173',
   optionsSuccessStatus: 200,
 };
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'MoodScribe API',
+            version: '1.0.0',
+            description: 'API documentation for MoodScribe'
+        },
+        servers: [
+            {
+                url: 'https://moodscribe.onrender.com',
+                description: 'Development server'
+            }
+        ],
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: 'http',
+                    scheme: 'bearer',
+                    bearerFormat: 'JWT'
+                }
+            }
+        },
+        security: [
+            {
+                bearerAuth: []
+            }
+        ]
+    },
+    apis: ['./routes/*.js']
+};
+  
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(cors(corsOptions));
 
